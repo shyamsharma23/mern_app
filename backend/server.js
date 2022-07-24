c491b08c23dd06fcd62ff2d0ef572d30a3a8d3aa
unknown <shyamsharma.sharma797@gmail.com>
2022-07-24 15:56:24 -0500

    Commit

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,20 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 
 connectDB();
 
+app.get('/', async (req, res) =>{
+    res.render('home')
+} )
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 
 app.use('/api/goals', require('./routes/goalsRoutes'))
